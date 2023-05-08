9761f2fb0ec3f59f071fd0b6b89a9569ef0b214e
Desace1938 <desaceves1714@gmail.com>
2023-05-08 16:33:15 -0500

    added challange

---
+++ 8Kyu/simple-multiplication.js
@@ -0,0 +1,6 @@
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+//My Solution
+function simpleMultiplication(number) {
+  return number % 2 == 0 ? number * 8 : number * 9
+}
