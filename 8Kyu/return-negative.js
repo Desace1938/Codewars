5d620999e668571ec2e101ae718446ba9f38a1ab
Desace1938 <desaceves1714@gmail.com>
2023-05-08 16:19:54 -0500

    code challenge

---
+++ 8Kyu/return-negative.js
@@ -0,0 +1,11 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+//my solution
+function makeNegative(num) {
+    if (num > 0){
+      return -num;
+    }else{
+      return num
+    }
+  }
