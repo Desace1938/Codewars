f56f2f3903a39080a7d10da33722d6abb23ef039
Desace1938 <desaceves1714@gmail.com>
2023-05-08 16:23:24 -0500

    boolean code challenge

---
+++ 8Kyu/convert-boolean.js
@@ -0,0 +1,11 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//My solution
+
+function boolToWord( bool ){
+    if ( bool){
+      return 'Yes';
+    }else{
+      return 'No';
+    }
+  }
