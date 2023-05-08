a85a06fc7f49c50f45e8e4ffede03bdbfa5c3eed
Desace1938 <desaceves1714@gmail.com>
2023-05-08 16:16:00 -0500

    another code challenge

---
+++ 8Kyu/string-repeat.js
@@ -0,0 +1,9 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+
+//My solution
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  
