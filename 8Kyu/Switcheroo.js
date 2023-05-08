11bc2754ff4b376df6d41507c7fa0f13ef7d2002
Desace1938 <desaceves1714@gmail.com>
2023-05-08 16:03:37 -0500

    codewar

---
+++ 8Kyu/Switcheroo.js
@@ -0,0 +1,20 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+
+
+//My solution 
+function switcheroo(x){
+  
+    //split the array and create a new one under map function 
+    return x.split('').map(function(e){
+      if ( e== 'b') return 'a';
+      if (e =='a') return 'b';
+      if (e =='c') return 'c';
+    // combine the new array with .join function
+    }).join('');
+  }
