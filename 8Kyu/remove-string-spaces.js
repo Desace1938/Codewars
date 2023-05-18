c84aceecf5df209ff542ec8c6f9d33f2cb1e7d35
Desace1938 <desaceves1714@gmail.com>
2023-05-18 13:45:09 -0500

    added string code

---
+++ 8Kyu/remove-string-spaces.js
@@ -0,0 +1,9 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+
+//My Solution
+function noSpace(x){
+    //split removes what is between the ''.
+    //join combines the strings into a new array.
+    return x = x.split(' ').join('');
+  }
