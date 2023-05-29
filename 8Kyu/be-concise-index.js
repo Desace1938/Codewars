5d1c758c70a4c63c01152e76a8627492b3d215d7
Desace1938 <desaceves1714@gmail.com>
2023-05-29 10:04:35 -0500

    convert strings to uppercase

---
+++ 8Kyu/be-concise-index.js
@@ -0,0 +1,16 @@
+// Be Concise IV - Index of an element in an array
+// Task
+// Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 161) You may assume that all array elements are unique.
+
+
+// function find(array, element) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === element) return i;
+//   }
+//   return "Not found";
+// }
+
+
+//My Solution
+const find = (arr, el) =>
+  arr.includes(el) ? arr.indexOf(el) : `Not found`;
