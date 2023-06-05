a133bcf79f54a75d368ef9645dcb100d24ab8ef7
Desace1938 <desaceves1714@gmail.com>
2023-06-05 10:56:02 -0500

    smallest integer finder challenge

---
+++ 8Kyu/find-smallest-integer.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+
+
+//My Solution
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let smallestInteger;
+   
+      for (let i = 0; i < args.length; i++){
+          if ( args[i] <= args[0] ) {
+              args[0]  = args[i];    
+              smallestInteger = args[i];
+          }
+        }
+      return smallestInteger;
+    }
+  }
+  
