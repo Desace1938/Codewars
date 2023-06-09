7b5f13c7c13e9bd82e73e5b77ee0cb231bab4bd5
Desace1938 <desaceves1714@gmail.com>
2023-06-09 10:24:47 -0500

    Used if/else to convert boolean to string

---
+++ 8Kyu/convert-boolean-to-string.js
@@ -0,0 +1,14 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+//My Solution
+
+function booleanToString(b){
+    if (b === true){
+      return "true";
+    }else{
+      return "false";
+    }
+  }
