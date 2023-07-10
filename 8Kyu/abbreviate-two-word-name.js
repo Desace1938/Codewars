991136721ef9f53f6c635d22de6bc1270eb06395
Desace1938 <desaceves1714@gmail.com>
2023-07-10 12:25:58 -0500

    Codewar: Used split, charAt, join function to convert name to initials.

---
+++ 8Kyu/abbreviate-two-word-name.js
@@ -0,0 +1,18 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+
+//My Solution
+function abbrevName(name){
+    let firstLetter = name
+    .split(" ").map(word => word.charAt(0)).join(".").toUpperCase();
+  
+    return firstLetter;
+  }
