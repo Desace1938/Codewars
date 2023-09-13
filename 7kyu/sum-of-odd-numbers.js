cd23c84f57565cf7c9574d496d745812d8625be9
Desace1938 <desaceves1714@gmail.com>
2023-09-12 19:00:19 -0500

    code challenge using power of 3

---
+++ 7kyu/sum-of-odd-numbers.js
@@ -0,0 +1,10 @@
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+//My Solutions
+
+function rowSumOddNumbers(n) {
+	return n**3
+}
